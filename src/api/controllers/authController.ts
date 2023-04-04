12bd186db14bfad9e0871c704e61a647dcab0e4c
Johnkai Cortez <73104201+Johnkai2196@users.noreply.github.com>
2023-04-04 16:09:14 +0300

    code fix

--- src/api/controllers/authController.ts
+++ src/api/controllers/authController.ts
@@ -4,6 +4,7 @@ import CustomError from '../../classes/CustomError';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import LoginMessageResponse from '../../interfaces/LoginMessageResponse';
+import {OutputUser} from '../../interfaces/User';
 
 // TODO: Create login controller that creates a jwt token and returns it to the user
 const login = async (
@@ -24,10 +25,19 @@ const login = async (
     }
     //set expiration depending on the app requirements. For example if social media app then no need, if banking app then 1 hour
     const token = jwt.sign({id: user._id}, process.env.JWT_SECRET as string);
+
+    const outputUser: OutputUser = {
+      user_name: user.user_name,
+      email: user.email,
+      id: user._id,
+    };
+
     const message: LoginMessageResponse = {
       message: 'Login successful',
       token,
+      user: outputUser,
     };
+
     res.json(message);
   } catch (error) {
     next(new CustomError((error as Error).message, 500));
